9debcfbe30b724abb14d1369352aebb01593f582
StuartIanNaylor <stuartiannaylor@outlook.com>
2021-05-18 18:08:23 +0100

    tfl-stream

--- tfl-stream.py
+++ tfl-stream.py
@@ -3,34 +3,7 @@ import sounddevice as sd
 import numpy as np
 import threading
 
-# Parameters
-word_threshold = 7.5
-word_duration = 10
-rec_duration = 0.020
-sample_rate = 16000
-num_channels = 1
 
-sd.default.latency= ('high', 'high')
-sd.default.dtype= ('float32', 'float32')
-
-
-# Load the TFLite model and allocate tensors.
-interpreter1 = tf.lite.Interpreter(model_path="/home/pi/g-kws/models2/crnn_state/quantize_opt_for_size_tflite_stream_state_external/stream_state_external.tflite")
-interpreter1.allocate_tensors()
-
-# Get input and output tensors.
-input_details1 = interpreter1.get_input_details()
-output_details1 = interpreter1.get_output_details()
-
-inputs1 = []
-
-for s in range(len(input_details1)):
-  inputs1.append(np.zeros(input_details1[s]['shape'], dtype=np.float32))
-    
-kw_count = 0
-not_kw_count = 0
-kw_sum = 0
-kw_hit = False
 def sd_callback(rec, frames, time, status):
     global input_details1
     global output_details1
@@ -39,6 +12,10 @@ def sd_callback(rec, frames, time, status):
     global not_kw_count
     global kw_sum
     global kw_hit
+    global kw_max
+    global kw_avg
+    global kw_probability
+
     # Notify if errors
     if status:
         print('Error:', status)
@@ -62,23 +39,65 @@ def sd_callback(rec, frames, time, status):
      
     if np.argmax(output_data[0]) == 2:
       if kw_count > 3:
-        print(output_data[0][2], kw_count, kw_sum)
+        print(output_data[0][0], output_data[0][1], output_data[0][2], kw_count, kw_sum)
         not_kw_count = 0
+        if output_data[0][2] > kw_max:
+          kw_max = output_data[0][2]
       kw_count += 1
       kw_sum = kw_sum + output_data[0][2]
-      if kw_sum > 450:
-        print("Kw threshold hit")
+      kw_avg = kw_sum / kw_count
+      if (kw_sum / kw_avg) / 55 > 1:
+        kw_probability = 1.0
+      else:
+        kw_probability = (kw_sum / kw_avg)  / 55
+      if kw_probability > 0.5:
         kw_hit = True
     elif np.argmax(output_data[0]) != 2:
-      if kw_hit:
-        for s in range(len(input_details1)):
-          inputs1[s] = np.zeros(input_details1[s]['shape'], dtype=np.float32)
-        kw_hit = False
       if not_kw_count > 3:
+        if kw_hit == True:
+          print("Kw threshold hit", kw_max, kw_avg, kw_probability)
         kw_count = 0
         kw_sum = 0
+        kw_hit = False
+        kw_max = 0
+        kw_probability = 0
         not_kw_count = -1
       not_kw_count += 1
+
+
+# Parameters
+word_threshold = 7.5
+word_duration = 10
+rec_duration = 0.020
+sample_rate = 16000
+num_channels = 1
+kw_max = 0
+kw_avg = 0
+kw_probability = 0
+
+sd.default.latency= ('high', 'high')
+sd.default.dtype= ('float32', 'float32')
+
+
+# Load the TFLite model and allocate tensors.
+interpreter1 = tf.lite.Interpreter(model_path="/home/pi/g-kws/models2/crnn_state/quantize_opt_for_size_tflite_stream_state_external/stream_state_external.tflite")
+interpreter1.allocate_tensors()
+
+# Get input and output tensors.
+input_details1 = interpreter1.get_input_details()
+output_details1 = interpreter1.get_output_details()
+
+inputs1 = []
+
+for s in range(len(input_details1)):
+  inputs1.append(np.zeros(input_details1[s]['shape'], dtype=np.float32))
+    
+kw_count = 0
+not_kw_count = 0
+kw_sum = 0
+kw_hit = False
+
+
 
     
 
